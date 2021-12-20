e511d573dba20b4e47c7f68070e5a2cd68d9ba60
徐梦强 <youxiubiji@gmail.com>
2021-12-20 10:53:39 +0800

    修改：完善登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,7 +6,26 @@
                 <div class="title">实时共享交换平台</div>
                 <div class="en">Real time shared switching platform</div>
             </div>
-            <div class="login-main_rg"></div>
+            <div class="login-main_rg">
+                <div class="login-name">登录</div>
+                <div class="login-form">
+                    <el-form ref="form" :model="loginForm" :rules="rules">
+                        <el-form-item prop="username">
+                            <el-input class="login-input" placeholder="请输入账号" clearable v-model="loginForm.username">
+                                <img slot="prefix" class="input-icon" src="../../assets/username_new.png" alt="" />
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item prop="password">
+                            <el-input class="login-input" placeholder="请输入密码" clearable show-password v-model="loginForm.password">
+                                <img slot="prefix" class="input-icon" src="../../assets/password_new.png" alt="" />
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" round class="login-btn" @click="onSubmit">登录</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,7 +33,19 @@
 export default {
     name: 'Login',
     data() {
-        return {}
+        return {
+            loginForm: {
+                username: '',
+                password: '',
+            },
+            rules: {
+                username: [{ required: true, message: '请输入账号', trigger: 'blur' }],
+                password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+            },
+        }
+    },
+    methods: {
+        onSubmit() {},
     },
 }
 </script>
@@ -42,6 +73,7 @@ export default {
             height: 551px;
             background: linear-gradient(-40deg, #24c7fc, #0349fa);
             border-radius: 15px 0px 0px 15px;
+            opacity: 0.8;
             .line {
                 margin: 44px 0 0 55px;
                 width: 29px;
@@ -59,12 +91,82 @@ export default {
                 font-size: 16px;
                 font-weight: 400;
                 color: #ffffff;
+                text-transform: uppercase;
             }
         }
         &_rg {
             width: 484px;
             height: 551px;
+            .login-name {
+                padding-top: 69px;
+                text-align: center;
+                font-size: 22px;
+                font-weight: bold;
+                color: #0599df;
+                position: relative;
+                z-index: 2;
+                &::after {
+                    content: '';
+                    position: absolute;
+                    bottom: 4px;
+                    left: 50%;
+                    width: 47px;
+                    height: 4px;
+                    transform: translateX(-50%);
+                    background: #0076fe;
+                    opacity: 0.2;
+                    z-index: 1;
+                }
+            }
+            .login-form {
+                margin: 58px 69px 0 64px;
+                .input-icon {
+                    width: 16px;
+                    height: 18px;
+                    margin: 17px 0 0 20px;
+                }
+                .login-btn {
+                    margin-top: 10px;
+                    width: 351px;
+                    height: 54px;
+                    background: #039de3;
+                    border-radius: 27px;
+                    font-size: 18px;
+                    font-weight: 500;
+                    color: #ffffff;
+                }
+            }
         }
+    }
+}
+
+::v-deep {
+    .el-input__inner {
+        width: 351px;
+        height: 54px;
+        border-radius: 27px;
+        padding-left: 53px;
+        font-size: 18px;
+        font-weight: 500;
+        color: #333;
+        margin-bottom: 10px;
+        &::placeholder {
+            font-size: 15px;
+            font-weight: 400;
+            color: #b9bed1;
+        }
+    }
+    .el-input__suffix {
+        right: 22px;
+        transform: translateY(-5px);
+    }
+    .el-input .el-input__clear {
+        font-size: 16px;
+    }
+    .el-form-item__error {
+        font-size: 14px;
+        top: 90%;
+        left: 0;
     }
 }
 </style>
