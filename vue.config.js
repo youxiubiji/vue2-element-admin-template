9244fc8172b26e1dabbd4bd9190631618cadc544
徐梦强 <youxiubiji@gmail.com>
2021-12-27 16:56:59 +0800

    修改：readme文档

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ function resolve(dir) {
 }
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue2-element-admin-template/' : '/',
     chainWebpack(config) {
         // set svg-sprite-loader
         config.module.rule('svg').exclude.add(resolve('src/icons')).end()
